5108acb4d24cadd274e7e1a049381b89e42557ab
qwewerqwewer1 <69668851+qwewerqwewer1@users.noreply.github.com>
2020-11-03 17:04:40 +0300

    test

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__close');
+let popupForm = document.querySelector('.popup__form');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let popupInputTitle = document.querySelector('.popup__input-title');
+let popupInputSubtitle = document.querySelector('.popup__input-subtitle');
+
+function openPopup () {
+  popup.classList.add('popup__opened');
+  popupInputTitle.value = profileTitle.textContent;
+  popupInputSubtitle.value = profileSubtitle.textContent;
+};
+
+function closePopup () {
+  popup.classList.remove('popup__opened');
+};
+
+function saveChanges (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = popupInputTitle.value;
+  profileSubtitle.textContent = popupInputSubtitle.value;
+  closePopup();
+};
+
+popupForm.addEventListener('submit', saveChanges);
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
